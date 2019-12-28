5dc17b87cf879cb33f4ac2218181639208d86eb0
David Lucas <davidlucadl99@gmail.com>
2019-12-27 22:14:44 -0500

    Otimized Pilha

--- pilha.py
+++ pilha.py
@@ -2,13 +2,16 @@ class Stack():
 
     def __init__(self):
         self.stack = []
+        self.len_stack = 0
 
     def push(self, e):
         self.stack.append(e)
+        self.len_stack += 1
 
     def pop(self):
         if not self.empty():
-            self.stack.pop(len(self.stack) - 1)
+            self.stack.pop(self.len_stack - 1)
+            self.len_stack -= 1
 
     def top(self):
         if not self.empty():
@@ -16,12 +19,12 @@ class Stack():
         return None
 
     def empty(self):
-        if (len(self.stack) == 0):
+        if self.len_stack == 0:
             return True
         return False
     
     def length(self):
-        return len(self.stack)
+        return self.len_stack
 
 s = Stack()
 
